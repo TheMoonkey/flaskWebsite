c6d558ae29232ff291c10868fc76f4223e9b5f23
TheMoonkey <397672278@qq.com>
2024-08-20 18:01:02 +0000

    first web succeed

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hello world!")
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def helloWorld():
+  return "Hello Word!"
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
